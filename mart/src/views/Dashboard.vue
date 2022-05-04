e088c640ea3a08800eee913fe62b72e2ac57beac
Achufam24 <achuulimagbama@mail.com>
2022-05-04 19:07:30 +0100

    second commit

---
+++ mart/src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="dashboard">
+     <h1>WeLCOME TO YOUR DASHOBOARD</h1>
+    <P>We are working on it at the moment.</P>
+    <p>For Now pls check AgroMart other Features</p>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.dashboard{
+    background: #e8e8e8;
+    height: 100vw;
+    color: green;
+}
+</style>
